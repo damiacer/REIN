0880201cbe78de08e4f2f2ce13f598bb9dc2096d
Damiano C <51063036+damiacer@users.noreply.github.com>
2023-11-23 15:46:41 +0100

    Update code22nov23.R

--- METHODS/HazReg/code22nov23.R
+++ METHODS/HazReg/code22nov23.R
@@ -15,6 +15,8 @@ library("here")
 library("gapminder")
 #install.packages("mitml")
 library("mitml")
+#install.packages("knitr")
+library("knitr")
 
 #-DATABASE "REIN"----------------------------------------------------------------
 rein <- read.csv2("rein_db.csv", header = TRUE, na.string="NA")
@@ -836,7 +838,7 @@ dim(rdb) # 40980 110
 
 #-no missing value dataset----
 
-#-small dataste
+#-smaller dataset
 
 rdb = subset(rdb, select = c(EVENT, time, cardiovasc, tabac2,
                         dial, apkd01, bmic, sex, age, diabetes))
@@ -844,7 +846,7 @@ rdb = subset(rdb, select = c(EVENT, time, cardiovasc, tabac2,
 dim(rdb) # 40980   10
 rdb = na.omit(rdb)
 
-#-HAZREG
+#-HAZREG package----
 
 # install.packages("devtools")
 require("devtools")
@@ -942,3 +944,150 @@ rownames(MLES) <- c("theta[1]","theta[2]","theta[3]","age_t", "age","cardiovasc"
 # MLEs for GH models
 # theta[1] in GGGH < 0.001, which is the reason why the rounded value appears as 0.000
 kable(MLES, digits = 3)
+
+# model comparaison----
+
+# AIC for models with PGW baseline hazard
+AICPGWGH <- 2*OPTPGWGH$OPT$objective + 2*length(OPTPGWGH$OPT$par)
+AICPGWAFT <- 2*OPTPGWAFT$OPT$objective + 2*length(OPTPGWAFT$OPT$par)
+AICPGWPH <- 2*OPTPGWPH$OPT$objective + 2*length(OPTPGWPH$OPT$par)
+AICPGWAH <- 2*OPTPGWAH$OPT$objective + 2*length(OPTPGWAH$OPT$par)
+
+# AICs for models with LL baseline hazard
+AICLLGH <- 2*OPTLLGH$OPT$objective + 2*length(OPTLLGH$OPT$par)
+AICLLAFT <- 2*OPTLLAFT$OPT$objective + 2*length(OPTLLAFT$OPT$par)
+AICLLPH <- 2*OPTLLPH$OPT$objective + 2*length(OPTLLPH$OPT$par)
+AICLLAH <- 2*OPTLLAH$OPT$objective + 2*length(OPTLLAH$OPT$par)
+
+# AICs for GH models with GG, EW, LN, and G hazards
+AICGGGH <- 2*OPTGGGH$OPT$objective + 2*length(OPTGGGH$OPT$par)
+AICEWGH <- 2*OPTEWGH$OPT$objective + 2*length(OPTEWGH$OPT$par)
+AICLNGH <- 2*OPTLNGH$OPT$objective + 2*length(OPTLNGH$OPT$par)
+AICGGH <- 2*OPTGGH$OPT$objective + 2*length(OPTGGH$OPT$par)
+
+
+# All AICs
+AICs <- c(AICPGWGH, AICPGWAFT, AICPGWPH, AICPGWAH,
+          AICLLGH, AICLLAFT, AICLLPH, AICLLAH,
+          AICGGGH, AICEWGH, AICLNGH, AICGGH)
+
+round(AICs, digits = 2)
+
+# Best model:
+which.min(AICs) # EWGH
+
+# Baseline hazards for GH models----
+
+# Fitted baseline hazard functions for GH models
+PGWGHhaz <- Vectorize(function(t) hpgw(t, MLEPGWGH[1], MLEPGWGH[2], MLEPGWGH[3]) ) 
+EWGHhaz <- Vectorize(function(t) hew(t, MLEEWGH[1], MLEEWGH[2], MLEEWGH[3]) ) 
+GGGHhaz <- Vectorize(function(t) hggamma(t, MLEGGGH[1], MLEGGGH[2], MLEGGGH[3]) ) 
+GGHhaz <- Vectorize(function(t) hgamma(t, MLEGGH[1], MLEGGH[2]) ) 
+LNGHhaz <- Vectorize(function(t) hlnorm(t, MLELNGH[1], MLELNGH[2])) 
+LLGHhaz <- Vectorize(function(t) hllogis(t, MLELLGH[1], MLELLGH[2])) 
+
+# Note that the baseline hazards associated to the top models look similar
+curve(PGWGHhaz,1e-6, max(times), xlab = "Time (years)", ylab = "Baseline Hazard", main = "",
+      cex.axis = 1.5, cex.lab = 1.5, lwd = 2, ylim = c(0,0.03), n = 1000)
+curve(EWGHhaz,1e-6, max(times), lwd = 2, n = 1000, lty = 2, add = TRUE) 
+curve(GGGHhaz,1e-6, max(times), lwd = 2, n = 1000, lty = 3, add = TRUE) 
+curve(GGHhaz,1e-6, max(times), lwd = 2, n = 1000, lty = 4, add = TRUE) 
+curve(LNGHhaz,1e-6, max(times), lwd = 2, n = 1000, lty = 5, add = TRUE) 
+curve(LLGHhaz,1e-6, max(times), lwd = 2, n = 1000, lty = 6, add = TRUE) 
+legend("topright", legend = c("PGWGH", "EWGH", "GGGH", "GGH", "LNGH", "LLGH"), lwd = rep(2,6), lty = 1:6)
+
+# best model summary
+
+# MLE in the original parameterisation
+MLE <- c(OPTLLGH$OPT$par[1], exp(OPTLLGH$OPT$par[2]), OPTLLGH$OPT$par[-c(1,2)])
+round(MLE, digits = 3)
+
+# 95% Confidence intervals under the reparameterisation
+require("numDeriv")
+CI <- Conf_Int(FUN = OPTLLGH$log_lik, MLE = OPTLLGH$OPT$par, level = 0.95)
+rownames(CI) <- c("theta[1]","theta[2]","theta[3]","age","cardiovasc",
+                  "tabac2","dial", "apkd01",
+                  "bmic","sex", "diabetes")
+
+kable(CI, digits = 3)
+
+# Fitted baseline hazard function
+fit_haz <- Vectorize(function(t) hllogis(t, MLE[1], MLE[2])) 
+
+curve(fit_haz,0.001, max(times), xlab = "Time (years)", ylab = "Baseline Hazard", main = "",
+      cex.axis = 1.5, cex.lab = 1.5, lwd = 2, ylim = c(0,0.04), n = 1000)
+
+# Average population survival function and KM estimator
+require("survival")
+
+pop_surv <- Vectorize(function(t){
+  p0 <- dim(Xt)[2]
+  p1 <- dim(X)[2]
+  theta1 <- MLE[1]; theta2 <- MLE[2]; alpha <- MLE[3:(2+p0)]; beta <- MLE[(3+p0):(2+p0+p1)]
+  x.alpha <- Xt%*%alpha
+  x.dif <- X%*%beta - x.alpha
+  out <- mean( exp( - chllogis(t*exp(x.alpha), theta1, theta2)*exp(x.dif)  )  )
+  return(out)
+})
+
+
+# Kaplan-Meier estimator 
+km <- survfit(Surv(times, status) ~ 1)
+
+# Comparison
+plot(km$time, km$surv, type = "l", col = "black", lwd = 2, lty = 1, ylim = c(0,1),
+     xlab = "Time (years)", ylab = "Population Survival", main = "",
+     cex.axis = 1.5, cex.lab = 1.5)
+curve(pop_surv,0.001,14, lwd = 2, n = 1000, add = TRUE, lty = 2, col = "gray")
+legend("bottomright", legend = c("KM","Parametric"), col = c("black","gray"), lwd = c(2,2), lty = c(1,2))
+
+# Confidence intervals for the survival function based on a normal approximation
+# at specific time points t0
+
+require("SimDesign")
+
+# Hessian and asymptotic covariance matrix
+HESS <- hessian(func = OPTLLGH$log_lik, x = OPTLLGH$OPT$par)
+Sigma <- solve(HESS)
+
+# Reparameterised MLE 
+r.MLE <- OPTLLGH$OPT$par
+
+# The function to obtain approximate CIs based on Monte Carlo simulations 
+# from the asymptotic normal distribution of the MLEs
+# t0 : time where the confidence interval will be calculated
+# level : confidence level
+# n.mc : number of Monte Carlo iterations
+
+conf.int.surv <- function(t0, level, n.mc){
+  p0 <- dim(Xt)[2]
+  p1 <- dim(X)[2]
+  mc <- vector()
+  S.par <- function(par){ mean( exp( - chllogis(t0*exp(Xt%*%par[3:(2+p0)]), par[1], par[2])*
+                                       exp(X%*%par[(3+p0):(2+p0+p1)]-Xt%*%par[3:(2+p0)])  )  )
+  }
+  
+  for(i in 1:n.mc) {
+    val <- rmvnorm(1,mean = r.MLE, sigma = Sigma)
+    val[2] <- exp(val[2])
+    mc[i] <- S.par(val)
+  }
+  
+  L <- quantile(mc,(1-level)*0.5)
+  U <- quantile(mc,(1+level)*0.5)
+  
+  M <- S.par(MLE)
+  
+  return(c(L,M,U))
+}
+
+
+# times for CIs calculations
+timesCI <- c(1,2.5,5,7.5,10,12.5)
+
+CIS <- matrix(0, ncol = 4, nrow = length(timesCI))
+
+for(k in 1:length(timesCI)) CIS[k,] <- c(timesCI[k],conf.int.surv(timesCI[k],0.95,10000))
+
+colnames(CIS) <- cbind("year","lower","population survival","upper")
+print(kable(CIS,digits=4))
